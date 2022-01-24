205eee528ea1d700d02218eaa31e30e9a1e7ab1f
mrMekdad <mekdadr6@gmail.com>
2022-01-24 02:16:00 +0100

    bulk-rnaseq-pipeline: add repository metadata and packaging

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name="reda-2022-bulk-rnaseq-pipeline",
+    version="0.1.0",
+    description="Repository for bulk RNAseq orchestration, naming rules, and reporting artifacts.",
+    author="Red@",
+    packages=["bulk_rnaseq_pipeline"],
+    package_dir={"": "src"},
+)
